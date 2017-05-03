895049d0780ddce0144f30157afad7271e72f98b
Ridwan A. Otun <codesmear@outlook.com>
2017-05-03 13:58:24 +0100

    commited articles files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles #tells rails that we have a resource named articles and the router should enable RESTful pattern
 end
